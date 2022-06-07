4469a8c76b825fd6954a7f3b757612d20c9b17aa
haeun <yoohaaeun@gmail.com>
2022-06-07 16:56:01 +0900

    ADD: ourTrip 초기세팅 완료

---
+++ src/pages/ReservationList/ReservationList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Reservationlist = () => {
+  return <div></div>;
+};
+
+export default Reservationlist;
---
+++ src/styles/Theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  white: '#FFFFFF',
+  kakaoYellow: '#FFD66C',
+  kakaoBrown: '#341F20',
+  lightGray: '#AEB5BC',
+  gray: '#676D74',
+  boldGray: '#353A3F',
+  blue: '#3FA5F7',
+  hoverBlue: '#4A97E6',
+  underlineBlue: '#88BEF0',
+};
+
+export default theme;
