6c61914eda3a184680b325691242d4f320e99486
cham-min <hymn9613@gmail.com>
2022-06-16 17:38:35 +0900

    QueryParameter 활용한 로딩페이지 구현 - setTimeout() 활용하여 로딩페이지 구현 (보여주기 위해서 하나의 페이지로 구현) - queryParameter에서 사용자에게 정보를 띄워주는 기능 구현

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 const Nav = () => {
   const [category, setCategory] = useState(0);
   const location = useLocation();
-  const navigate = useNavigate();
+  const navigate = useNavigate;
   const goToLogin = () => {
     navigate('/login');
   };
