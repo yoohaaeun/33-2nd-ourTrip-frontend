8a33aada9662a07b286824fac45087b78817ebf7
cham-min <hymn9613@gmail.com>
2022-06-16 17:38:35 +0900

    QueryParameter 활용한 로딩페이지 구현 - setTimeout() 활용하여 로딩페이지 구현 (보여주기 위해서 하나의 페이지로 구현) - queryParameter에서 사용자에게 정보를 띄워주는 기능 구현

--- src/pages/Loading/LoadingPage.js
+++ src/pages/Loading/LoadingPage.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 function LoadingPage() {
+  const navigate = useNavigate();
+  const urlDocument = new URL(document.location);
+  const params = urlDocument.searchParams;
+  const query = urlDocument.search;
+  const date = params.get('date');
+  const origin = params.get('origin');
+  const destination = params.get('destination');
+
+  const navigateFlight = () => {
+    navigate(`/flights${query}`);
+  };
+
+  setTimeout(() => navigateFlight(), 4000);
+
   return (
     <LoadingCover>
       <LoadingImage />
       <LoadingBackground />
       <LoadingContainer>
         <LoadingTitle>
-          <LoadingTitleText>김포에서 제주까지</LoadingTitleText>
           <LoadingTitleText>
-            <span>왕복 항공권을 찾고 있습니다.</span>
+            {origin}에서 {destination}까지
+          </LoadingTitleText>
+          <LoadingTitleText>
+            <span>편도 항공권을 찾고 있습니다.</span>
           </LoadingTitleText>
         </LoadingTitle>
         <LoadingData>
           <LoadingLocation>
-            <LoadingEngTitle>GMP</LoadingEngTitle>
-            <LoadingKorTitle>김포</LoadingKorTitle>
-            <LoadingTime>2022년 06월 21일</LoadingTime>
+            <LoadingEngTitle>{`${AIR_INITIAL[origin]}`}</LoadingEngTitle>
+            <LoadingKorTitle>{origin}</LoadingKorTitle>
+            <LoadingTime>{date}</LoadingTime>
           </LoadingLocation>
           <LoadingLocation>
-            <LoadingEngTitle>CJU</LoadingEngTitle>
-            <LoadingKorTitle>제주</LoadingKorTitle>
-            <LoadingTime>2022년 06월 21일</LoadingTime>
+            <LoadingEngTitle>{`${AIR_INITIAL[destination]}`}</LoadingEngTitle>
+            <LoadingKorTitle>{destination}</LoadingKorTitle>
+            <LoadingTime>{date}</LoadingTime>
           </LoadingLocation>
         </LoadingData>
         <LoadingContent>
@@ -36,6 +52,21 @@ function LoadingPage() {
     </LoadingCover>
   );
 }
+
+const AIR_INITIAL = {
+  제주: 'CJU',
+  김포: 'GMP',
+  부산: 'PUS',
+  청주: 'CJJ',
+  여수: 'RSU',
+  광주: 'KWJ',
+  대구: 'TAE',
+  양양: 'YNY',
+  군산: 'KUV',
+  울산: 'USN',
+  포항: 'KPO',
+  인천: 'ICN',
+};
 
 const LoadingCover = styled.div`
   ${({ theme }) => theme.flex.flexBox('column')};
@@ -54,6 +85,7 @@ const LoadingImage = styled.div`
   width: 100%;
   background: url('images/loadingImg.jpg') 0/ 110%;
   background-size: full;
+  background-repeat: no-repeat;
   @keyframes moveAirplane {
     0% {
       background-position: 0;
